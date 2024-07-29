b0d10103f494e68ffb9e3b1e4084b6398bea906c
Ravuthz <ravuthz@gmail.com>
2024-07-29 09:18:39 +0700

    Disable mapResponse

--- src/app/index.ts
+++ src/app/index.ts
@@ -35,44 +35,40 @@ export type ErrorType = keyof typeof statusCodes;
 
 const app = new Elysia({ prefix: '/api' })
     .onAfterResponse((res: any) => {
-        if (import.meta.env.DEBUG_REQUEST) {
+        if (import.meta.env?.DEBUG_REQUEST) {
             console.log('\n\n', res.request.method, res.url, performance.now(), res.set.status)
         }
     })
-    .mapResponse((res: any) => {
-        const { response, set } = res;
-        const isJson = typeof response === 'object'
+    // .mapResponse(({ set, error, response }: any) => {
 
-        let text: string
-        let contentType: string
+    //     let text: string
+    //     let contentType: string
 
-        if (isJson) {
-            let json: any = {}
+    //     if (typeof response === 'object') {
+    //         let json: any = {}
+    //         if (set.status >= 200 && set.status <= 299) {
+    //             json['data'] = response
+    //             json['status'] = set.status || 200
+    //             json['message'] = response.message || statusCodes.OK.message
+    //         } else {
+    //             json['error'] = error
+    //             json['status'] = set.status || 500
+    //             json['message'] = response.message || statusCodes.INTERNAL_SERVER_ERROR.message
+    //         }
+    //         text = JSON.stringify(json)
+    //         contentType = 'application/json'
+    //     } else {
+    //         text = response?.toString() ?? ''
+    //         contentType = 'text/plain'
+    //     }
 
-            if (set.status >= 200 && set.status <= 299) {
-                json['data'] = response
-                json['status'] = set.status || 200
-                json['message'] = response.message || statusCodes.OK.message
-            } else {
-                json['error'] = res.error
-                json['status'] = set.status || 500
-                json['message'] = response.message || statusCodes.INTERNAL_SERVER_ERROR.message
-            }
-
-            text = JSON.stringify(json)
-            contentType = 'application/json'
-        } else {
-            text = response?.toString() ?? ''
-            contentType = 'text/plain'
-        }
-
-        return new Response(
-            text,
-            {
-                headers: { 'Content-Type': `${contentType}; charset=utf-8` }
-            }
-        )
-    })
+    //     return new Response(
+    //         text,
+    //         {
+    //             headers: { 'Content-Type': `${contentType}; charset=utf-8` }
+    //         }
+    //     )
+    // })
     .get('/', () => 'Hello, Elysia!')
     .use(swagger({
         path: '/api-docs',
